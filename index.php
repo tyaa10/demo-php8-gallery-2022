5101fa4c016356bf2c0d6faaa92f0cc54da87ceb
YuriiTrofimenko <tyaa@ukr.net>
2022-01-12 20:13:42 +0200

    navbar implementation

--- index.php
+++ index.php
@@ -4,9 +4,40 @@
     <meta charset="UTF-8">
     <title>Demo PHP Gallery</title>
     <link rel="stylesheet" href="frontend/node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="frontend/css/style.css">
 </head>
 <body>
-    <div>Hello PHP Gallery!</div>
+    <div class="container">
+        <div class="row">
+            <header class="col-12"></header>
+        </div>
+        <div class="row">
+            <nav class="col-12 navbar navbar-expand-lg navbar-light bg-light">
+                <?php include_once 'pages/menu.php' ?>
+            </nav>
+        </div>
+        <div class="row">
+            <section class="col-12">
+                <?php
+                    $page = $_GET['page'] ?: 1;
+                    switch ($page) {
+                        case 1:
+                            include_once 'pages/home.php';
+                            break;
+                        case 2:
+                            include_once 'pages/upload.php';
+                            break;
+                        case 3:
+                            include_once 'pages/gallery.php';
+                            break;
+                        case 4:
+                            include_once 'pages/registration.php';
+                            break;
+                    }
+                ?>
+            </section>
+        </div>
+    </div>
     <script src="frontend/node_modules/jquery/dist/jquery.min.js"></script>
     <script src="frontend/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
---
+++ pages/menu.php
@@ -0,0 +1,20 @@
+<a class="navbar-brand" href="#">Demo PHP Gallery</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+        <li class="nav-item <?php echo (!isset($_GET['page']) || $_GET['page'] == 1) ? 'active' : ''; ?>">
+            <a class="nav-link" href="index.php?page=1">Home</a>
+        </li>
+        <li class="nav-item <?php echo $_GET['page'] == 2 ? 'active' : ''; ?>">
+            <a class="nav-link" href="index.php?page=2">Upload</a>
+        </li>
+        <li class="nav-item <?php echo $_GET['page'] == 3 ? 'active' : ''; ?>">
+            <a class="nav-link" href="index.php?page=3">Gallery</a>
+        </li>
+        <li class="nav-item <?php echo $_GET['page'] == 4 ? 'active' : ''; ?>">
+            <a class="nav-link" href="index.php?page=4">Registration</a>
+        </li>
+    </ul>
+</div>
