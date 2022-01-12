727e8db5f4587790641cb15bd9db60fa3d6f9890
YuriiTrofimenko <tyaa@ukr.net>
2022-01-12 18:57:47 +0200

    npm init, bootstrap 4

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Demo PHP Gallery</title>
+    <link rel="stylesheet" href="frontend/node_modules/bootstrap/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div>Hello PHP Gallery!</div>
+    <script src="frontend/node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="frontend/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
